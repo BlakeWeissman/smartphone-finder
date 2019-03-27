c1032357a35c674e146a97449003c0a74cfc77f2
Blake Weissman <email@blakeweissman.com>
2019-03-27 18:15:53 -0400

    Implemented price and images in the card component

--- src/Card.js
+++ src/Card.js
@@ -6,16 +6,17 @@ export class Card extends Component {
     return (
         <div className="card text-center">
             <div className="card-wrap">
-            <div className="card-image background-dark">
-                <a href={this.props.link} target="_blank"></a>
-            </div>
-            <div className="card-content">
-                <h2>{this.props.name}</h2>
-                <p>{this.props.desc}</p>
-                <div className="button-wrap">
-                    <a className="button1" href={this.props.link} target="_blank">Learn More</a>
+                <div className="card-image" style={{backgroundImage: "url(" + this.props.img + ")"}}>
+                    <a href={this.props.link} target="_blank"></a>
                 </div>
-            </div>
+                <div className="card-content">
+                    <h2 className="card-content">{this.props.name}</h2>         
+                    <h3 className="card-content">{this.props.price}</h3>
+                    <p className="card-content">{this.props.desc}</p>
+                    <div className="button-wrap">
+                        <a className="button1" href={this.props.link} target="_blank">Learn More</a>
+                    </div>
+                </div>
             </div>
         </div>
     );
