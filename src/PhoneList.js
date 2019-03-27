a68f86616622e6cb2534fa5c1c8479d8bc268022
Blake Weissman <email@blakeweissman.com>
2019-03-27 18:16:31 -0400

    Seperated the phone list into a seperate file

---
+++ src/PhoneList.js
@@ -0,0 +1,56 @@
+import { Phone } from "./Phone.js";
+
+export const PhoneList = [
+    new Phone(
+        "iPhone XS", 
+        "Welcome to the big screens. Super Retina in two sizes — including the largest display ever on an iPhone. The most powerful chip in a smartphone.", 
+        "https://www.apple.com/iphone-xs/", 
+        "img/iphonexs.jpg", 
+        "$999.99",
+        "iOS", 
+        "2018", 
+        "2436x1125", 
+        "Apple A12 Bionic", 
+        "4gb", 
+        false
+    ),
+    new Phone(
+        "Samsung Galaxy S10", 
+        "After 10 years of mobile pioneering firsts, it's time to meet our latest and greatest innovation yet. Introducing the Galaxy S10. The next generation has arrived.", 
+        "https://www.samsung.com/us/mobile/galaxy-s10/", 
+        "img/s10.jpg",
+        "$899.99", 
+        "Android", 
+        "2019", 
+        "3040x1440", 
+        "Snapdragon 855", 
+        "8gb", 
+        true
+    ),
+    new Phone(
+        "Google Pixel 3", 
+        "Meet the Google Pixel 3. Explore your world with Google Lens. Capture breathtaking photos using Portrait Mode. Screen phone calls and get answers with Google.", 
+        "https://store.google.com/us/product/pixel_3?hl=en-US", 
+        "img/pixel3.jpg", 
+        "$799.99",
+        "Android", 
+        "2018", 
+        "2160x1080", 
+        "Snapdragon 845", 
+        "4gb", 
+        false
+    ),
+    new Phone(
+        "OnePlus 6T", 
+        "OnePlus 6T comes equipped with Screen Unlock, an in-display fingerprint sensor technology. Prepare to Unlock The Speed with 8GB RAM and up to 256GB.", 
+        "https://www.oneplus.com/6t", 
+        "img/oneplus6t.jpg",
+        "$579.99", 
+        "Android", 
+        "2018", 
+        "2280x1080", 
+        "Snapdragon 845", 
+        "8gb", 
+        false
+    ),
+]
--- src/Result.js
+++ src/Result.js
@@ -1,24 +1,19 @@
 import React, { Component } from "react";
 import { Card } from "./Card.js";
-import { Phone } from "./Phone.js";
+import { PhoneList } from "./PhoneList.js"
 
 export class Result extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            phoneList: [
-                new Phone("iPhone XS", "DESC", "LINK", "IMG", "iOS", "2018", "2436x1125", "Apple A12 Bionic", "4gb", false),
-                new Phone("Samsung Galaxy S10", "DESC", "LINK", "IMG", "Android", "2019", "3040x1440", "Snapdragon 855", "8gb", true),
-                new Phone("Google Pixel 3", "DESC", "LINK", "IMG", "Android", "2018", "2160x1080", "Snapdragon 845", "4gb", false)
-            ]
+            phones: PhoneList
         }
     }
-
     render() {      
         return (
-            <div class="wrap flex">
-                {this.state.phoneList.map((phone) =>
-                    <Card name={phone.name} desc={phone.desc} link={phone.link} />
+            <div className="app-display wrap flex">
+                {this.state.phones.map((phone) =>
+                    <Card name={phone.name} desc={phone.desc} link={phone.link} img={phone.img} price={phone.price} />
                 )}
             </div>
         );
