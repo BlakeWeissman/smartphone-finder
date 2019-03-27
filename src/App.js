813d9c407e573f3b6d598dfde40f762d98e2b016
Blake Weissman <email@blakeweissman.com>
2019-03-27 11:47:20 -0400

    Created basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="app-header background-dark">
+          <div className="wrap">
+            <p className="color-light">header</p>
+          </div>
+        </div>
+        <div className="app-display">
+          <div className="wrap">
+            <p className="color-dark">display</p>
+          </div>
+        </div>
+        <footer className="background-dark">
+          <div className="wrap">
+            <p className="color-light">footer</p>
+          </div>
+        </footer>
       </div>
     );
   }
