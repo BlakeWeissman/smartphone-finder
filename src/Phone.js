d4da39cd7edad0a18a4a5521971b3a88b06b3ddf
Blake Weissman <email@blakeweissman.com>
2019-03-27 18:15:18 -0400

    Added a price prop to the phone class

--- src/Phone.js
+++ src/Phone.js
@@ -1,9 +1,10 @@
 export class Phone {
-    constructor(name, desc, link, img, os, year, res, size, cpu, ram, headphone) {
+    constructor(name, desc, link, img, price, os, year, res, size, cpu, ram, headphone) {
         this.name = name;
         this.desc = desc;
         this.link = link;
         this.img = img;
+        this.price = price;
         this.os = os;
         this.year = year;
         this.res = res;
