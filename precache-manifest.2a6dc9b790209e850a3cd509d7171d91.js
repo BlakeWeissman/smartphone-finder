efd64488a3e6d8d2878e11d2081280bd1d5ac55e
Blake Weissman <email@blakeweissman.com>
2019-03-27 22:30:13 -0400

    Updates

---
+++ precache-manifest.2a6dc9b790209e850a3cd509d7171d91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b3322d93b66c9f73f23",
+    "url": "/smartphone-finder/static/js/runtime~main.a2f529c1.js"
+  },
+  {
+    "revision": "6fca0c376fb30ce24377",
+    "url": "/smartphone-finder/static/js/main.0376076e.chunk.js"
+  },
+  {
+    "revision": "06606c16e5df8e94cd7e",
+    "url": "/smartphone-finder/static/js/2.e53388fd.chunk.js"
+  },
+  {
+    "revision": "6fca0c376fb30ce24377",
+    "url": "/smartphone-finder/static/css/main.4fbd8df0.chunk.css"
+  },
+  {
+    "revision": "301b3f847337639e9991625043a37120",
+    "url": "/smartphone-finder/index.html"
+  }
+];
